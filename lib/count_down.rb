6ce33a8c93b4e4fb00c57291a7ce0f535f0eddcd
Hugo Delgado <hugo.delgado1@outlook.com>
2020-07-27 05:41:31 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,30 +1,30 @@
 # Write your code here
-countdown = 10
- while countdown <= 0 do
-   countdown = countdown - 1
+count = 10
+ while count >= 0 do
+   count = count - 1
  end
 
- when countdown == 10 do
+ when count == 10 do
    puts "10"
- elsif countdown == 9
+ elsif count == 9
    puts "9"
- elsif countdown == 8
+ elsif count == 8
    puts "8"
- elsif countdown == 7
+ elsif count == 7
    puts "7"
- elsif countdown == 6
+ elsif count == 6
    puts "6"
- elsif countdown == 5
+ elsif count == 5
    puts "5"
- elsif countdown == 4
+ elsif count == 4
    puts "4"
- elsif countdown == 3
+ elsif count == 3
    puts "3"
- elsif countdown == 2
+ elsif count == 2
    puts "2"
- elsif countdown == 1
+ elsif count == 1
    puts "1"
- elsif countdown == 0
+ elsif count == 0
    puts "Happy New Year!"
  end
 
