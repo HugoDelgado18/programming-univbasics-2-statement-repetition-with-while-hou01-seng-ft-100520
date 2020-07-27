5937d63eba0dcc0e17fdb143af66c17b5a5b75c1
Hugo Delgado <hugo.delgado1@outlook.com>
2020-07-27 05:43:04 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -4,27 +4,5 @@ count = 10
    puts count
    count -= 1
  end
+ puts "Happy New Year!"
 
- when count == 10 do
-   puts "10"
- elsif count == 9
-   puts "9"
- elsif count == 8
-   puts "8"
- elsif count == 7
-   puts "7"
- elsif count == 6
-   puts "6"
- elsif count == 5
-   puts "5"
- elsif count == 4
-   puts "4"
- elsif count == 3
-   puts "3"
- elsif count == 2
-   puts "2"
- elsif count == 1
-   puts "1"
- elsif count == 0
-   puts "Happy New Year!"
- end
